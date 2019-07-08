ac29f2ded28ddca4a7a8b2fb935745cf21509b34
liulei <scutliulei@163.com>
2019-07-08 12:00:52 +0800

    neural_network

---
+++ pytorch_first_step/pytorch_neural_network.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/8 11:45
+# @Author  : Bryce Liu
+# @FileName: pytorch_neural_network.py
+
+# =====define neural network
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        # 继承父类init方法
+        super(Net, self).__init__()
+        # 卷积层1表示单通道输入图片，6表示输出通道数，5表示卷积核为5*5
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        # 卷积层
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # 防射层/全连接层, y = Wx +b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # 卷积 -> 激活 -> 池化
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        # reshape, -1表示自适应
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+print(net)
+"""
+Net(
+  (conv1): Conv2d(1, 6, kernel_size=(5, 5), stride=(1, 1))
+  (conv2): Conv2d(6, 16, kernel_size=(5, 5), stride=(1, 1))
+  (fc1): Linear(in_features=400, out_features=120, bias=True)
+  (fc2): Linear(in_features=120, out_features=84, bias=True)
+  (fc3): Linear(in_features=84, out_features=10, bias=True)
+)
+"""
