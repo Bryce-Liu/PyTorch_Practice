0cbab1fd3b4212931c0cb938de58268d016e17fc
liulei <scutliulei@163.com>
2019-08-11 11:39:08 +0800

    modify

--- start_numpy/3_create_array.py
+++ start_numpy/3_create_array.py
@@ -8,16 +8,19 @@ f_array = np.array([4, 5, 6], dtype=np.float64)  # 定义array与格式
 print(f_array)  # [4. 5. 6.]
 print(f_array.dtype)  # float64
 
+f_array = f_array.astype(np.int)
+print(f_array.dtype)  # int64
+
 dim2_array = np.array([[1, 2, 3],
                        [4, 5, 6]])
-print(dim2_array)
+# print(dim2_array)
 """
 [[1 2 3]
  [4 5 6]]
 """
 
 zero_array = np.zeros((3, 4))  # 全零矩阵
-print(zero_array)
+# print(zero_array)
 """
 [[0. 0. 0. 0.]
  [0. 0. 0. 0.]
@@ -25,7 +28,7 @@ print(zero_array)
 """
 
 one_array = np.ones((3, 4))  # 全1矩阵
-print(one_array)
+# print(one_array)
 """
 [[1. 1. 1. 1.]
  [1. 1. 1. 1.]
@@ -41,9 +44,9 @@ print(empty_array)
 """
 
 range_array = np.arange(0, 12, 2)  # 生成一维矩阵
-print(range_array)  # [ 0  2  4  6  8 10]
+# print(range_array)  # [ 0  2  4  6  8 10]
 temp_array = range_array.reshape((2, 3))  # 变换矩阵维度
-print(temp_array)
+# print(temp_array)
 """
 [[ 0  2  4]
  [ 6  8 10]]
@@ -51,9 +54,13 @@ print(temp_array)
 
 line_array = np.linspace(1, 10, 4)  # 生成线段
 print(line_array)  # [ 1.  4.  7. 10.]
+
+np.random.shuffle(line_array)
+print(line_array)  # [10.  7.  1.  4.]
+
 temp_array = line_array.reshape((2, 2))
 print(temp_array)
 """
-[[ 1.  4.]
- [ 7. 10.]]
+[[10.  7.]
+ [ 1.  4.]]
 """
